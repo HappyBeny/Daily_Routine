81c79bc7749ffe3453a735ff57678d1e47380d47
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-10-17 15:34:31 +0900

    자바 데일리루틴 7번 문제 풀이

---
+++ src/Division.java
@@ -0,0 +1,15 @@
+public class Division {
+    public static void main(String[] args) {
+
+        int result = solution(3,2);
+        System.out.println("result = " + result);
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+        double div = (double) num1 / num2;
+        double div2 = div*1000;
+        answer = (int)div2;
+        return answer;
+    }
+}
