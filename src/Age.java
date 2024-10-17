c2f0393e8fd13ae5458a2b4deedf02ce184cae99
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-10-17 15:23:35 +0900

    자바 데일리루틴 4번 문제 풀이

---
+++ src/Age.java
@@ -0,0 +1,13 @@
+public class Age {
+    public static void main(String[] args) {
+
+        int result = solution(23);
+        System.out.println("result = " + result);
+    }
+
+    public static int solution(int age) {
+        int answer = 0;
+        answer = 2022 - age + 1;
+        return answer;
+    }
+}
