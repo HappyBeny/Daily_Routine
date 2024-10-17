e53840f1e3ae3ed64bd6ae48cb06b18a8c4898db
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-10-17 15:04:52 +0900

    기존 수행한 데일리루틴 gradle에서 IntelliJ로 변경ㅠㅠ

---
+++ src/Avg.java
@@ -0,0 +1,29 @@
+public class Avg {
+    public static void main(String[] args) {
+        int[] numbers = new int[11];
+        numbers[0] = 89;
+        numbers[1] = 90;
+        numbers[2] = 91;
+        numbers[3] = 92;
+        numbers[4] = 93;
+        numbers[5] = 94;
+        numbers[6] = 95;
+        numbers[7] = 96;
+        numbers[8] = 97;
+        numbers[9] = 98;
+        numbers[10] = 99;
+
+        double result = solution(numbers);
+        System.out.println("result = " + result);
+    }
+    static double solution(int[] nums) {
+        double answer = 0;
+        double sum = 0;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+        }
+        answer = sum / nums.length;
+        return answer;
+    }
+}
+
