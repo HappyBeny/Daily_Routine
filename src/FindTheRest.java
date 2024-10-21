b4b4bc427c8cdd7a3b2118fa6e75f9e7fa732d22
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-10-21 19:54:37 +0900

    241021 자바 데일리루틴 15번 문제풀이

---
+++ src/FindTheRest.java
@@ -0,0 +1,18 @@
+public class FindTheRest {
+    public static void main(String[] args) {
+
+       int result = solution(10);
+        System.out.println("result = " + result);
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i=2; i<=n; i++){
+            if (n%i == 1){
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
