ca2bf91887f83a83a14502fc739fa6a8cdc2ad88
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-10-24 15:05:09 +0900

    241024 데일리루틴 18번 문제풀이

---
+++ src/StrToInt.java
@@ -0,0 +1,15 @@
+public class StrToInt {
+    public static void main(String[] args) {
+
+        String num = "-121234";
+        int ChangedNum = solution(num);
+        System.out.println("ChangedNum = " + ChangedNum);
+    }
+
+    public static int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+}
