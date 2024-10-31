db221335190a44fcb96f97051a282a59e133fca5
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-10-31 23:45:13 +0900

    241110 데일리루틴 35번 문제풀이

--- src/Nobemver/CalculateDeficitAmount.java
+++ src/Nobemver/CalculateDeficitAmount.java
@@ -1,4 +1,18 @@
 package Nobemver;
 
-public class SortStringDescending {
+public class CalculateDeficitAmount {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+        long totalPrice = 0;
+
+        for (int i = 1; i <= count; i++) {
+            totalPrice += price * i;
+        }
+        answer = totalPrice - money;
+
+        if (answer < 0) {
+            return 0;
+        }
+        return answer;
+    }
 }
