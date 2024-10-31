59392119adddb78e7bcf002d52b4948e69713783
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-01 00:39:41 +0900

    241114 데일리루틴 39번 문제풀이

--- src/Nobemver/GCDAndLCM.java
+++ src/Nobemver/GCDAndLCM.java
@@ -1,4 +1,33 @@
 package Nobemver;
 
-public class BasicStringManipulation {
+public class GCDAndLCM {
+    public static int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        int c;
+
+        if (n > m) {
+            c = n;
+            n = m;
+            m = c;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                if (m % i == 0) {
+                    answer[0] = i;
+                }
+            }
+        }
+
+        for (int i = m; i <= 2147483647; i++) {
+            if(i % m == 0){
+                if (i % n == 0){
+                    answer[1] = i;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
 }
