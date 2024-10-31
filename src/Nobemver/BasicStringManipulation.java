d3f071d249b8ca6216063bad49f81cfae689140d
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-01 00:04:12 +0900

    241111 데일리루틴 36번 문제풀이

--- src/Nobemver/BasicStringManipulation.java
+++ src/Nobemver/BasicStringManipulation.java
@@ -1,4 +1,18 @@
 package Nobemver;
 
-public class SortStringDescending {
+public class BasicStringManipulation {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() != 4 && s.length()!=6){
+            return false;
+        }
+
+        try{
+            Integer.parseInt(s);
+        }catch (Exception e){
+            answer = false;
+        }
+
+        return answer;
+    }
 }
