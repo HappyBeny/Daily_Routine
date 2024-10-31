0f1b56fbb09ec38fa7a9d697059c4b06ae3e03a1
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-01 02:12:40 +0900

    241116 데일리루틴 41번 문제풀이

--- src/Nobemver/ReverseTernary.java
+++ src/Nobemver/ReverseTernary.java
@@ -1,4 +1,21 @@
 package Nobemver;
 
-public class BasicStringManipulation {
+public class ReverseTernary {
+    public int solution(int n) {
+        int answer = 0;
+        StringBuilder ternary = new StringBuilder();
+        String terNum;
+
+        while (n > 0) {
+            ternary.append(n % 3);
+            n /= 3;
+        }
+        terNum = ternary.toString();
+
+        for (int i = 0; i < terNum.length(); i++) {
+            int num = Integer.parseInt(terNum.substring(terNum.length()-i-1, terNum.length()-i));
+            answer += num * (int) Math.pow(3, i);
+        }
+        return answer;
+    }
 }
