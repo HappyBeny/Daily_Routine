4ddc6b1810d71eb8939d5c3490850835c8d37e91
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-10-31 19:59:55 +0900

    241103 데일리루틴 28번 문제풀이

--- src/RemoveSmallestNumber.java
+++ src/RemoveSmallestNumber.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class RemoveSmallestNumber {
+public class RemoveSmallestNumber {
 }
--- src/WatermelonPattern.java
+++ src/WatermelonPattern.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class WatermelonPattern {
+public class WatermelonPattern {
 }
