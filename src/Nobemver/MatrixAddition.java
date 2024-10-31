e8c28586ecae5c6fb96fe4f5d20d4dfa0c246c13
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-01 00:10:18 +0900

    241112 데일리루틴 37번 문제풀이

--- src/Nobemver/MatrixAddition.java
+++ src/Nobemver/MatrixAddition.java
@@ -1,4 +1,18 @@
 package Nobemver;
 
-public class BasicStringManipulation {
+public class MatrixAddition {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int col = arr1[0].length;
+        int row = arr1.length;
+
+        int[][] answer = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        return answer;
+    }
 }
