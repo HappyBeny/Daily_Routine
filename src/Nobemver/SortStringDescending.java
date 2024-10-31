4d697507e248bad52cbbadef1ebde9b33884a78b
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-10-31 23:36:46 +0900

    241109 데일리루틴 34번 문제풀이

--- src/Nobemver/SortStringDescending.java
+++ src/Nobemver/SortStringDescending.java
@@ -1,4 +1,14 @@
 package Nobemver;
 
-public class CountDivisorsAndSum {
+import java.util.Arrays;
+
+public class SortStringDescending {
+    public String solution(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+
+        StringBuilder sb = new StringBuilder(new String(chars)).reverse();
+
+        return sb.toString();
+    }
 }
