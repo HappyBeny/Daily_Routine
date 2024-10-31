31d2541924d89f1e1edca574c788c08504832eaa
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-01 01:19:50 +0900

    241115 데일리루틴 40번 문제풀이

--- src/Nobemver/CreateStrangeString.java
+++ src/Nobemver/CreateStrangeString.java
@@ -1,4 +1,23 @@
 package Nobemver;
 
-public class BasicStringManipulation {
+public class CreateStrangeString {
+    public String solution(String s) {
+        String answer = "";
+        String[] words = s.split(" ", -1);
+        String[] word = new String[words.length];
+
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder sb = new StringBuilder("");
+            for (int j = 0; j < words[i].length(); j++) {
+                if (j % 2 == 0){
+                    sb.append(Character.toUpperCase(words[i].charAt(j)));
+                } else {
+                    sb.append(Character.toLowerCase(words[i].charAt(j)));
+                }
+            }
+            word[i] = sb.toString();
+        }
+        answer = String.join(" ", word);
+        return answer;
+    }
 }
