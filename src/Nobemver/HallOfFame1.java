585a6b0fb1594ea49975d756e12771a8120d4beb
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-26 13:43:30 +0900

    241128 데일리루틴 53번 이

--- src/Nobemver/HallOfFame1.java
+++ src/Nobemver/HallOfFame1.java
@@ -1,2 +1,22 @@
-package Nobemver;public class HallOfFame1 {
+package Nobemver;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class HallOfFame1 {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        ArrayList<Integer> hallOfFame = new ArrayList<>();
+
+        for (int i = 0; i < score.length; i++) {
+            hallOfFame.add(score[i]);
+            Collections.sort(hallOfFame);
+
+            if (hallOfFame.size() > k) {
+                hallOfFame.remove(0);
+            }
+            answer[i] = hallOfFame.get(0);
+        }
+        return answer;
+    }
 }
