8f017e4c4c2f883b72c86dc0feb56c74e9c8de51
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-02 10:54:49 +0900

    241201 데일리루틴 56번 문제풀이

--- src/December/FruitSeller.java
+++ src/December/FruitSeller.java
@@ -6,10 +6,8 @@ public class FruitSeller {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         Arrays.sort(score);
-        for (int i = score.length ; i >= m-1 ; i--) {
-            i -= m;
+        for (int i = score.length -m ; i >= 0 ; i-= m) {
             answer += score[i] * m;
-
         }
         return answer;
     }
